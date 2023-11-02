2d987ad09b845a35e662f163df9449d23d12ba52
tatyanaskoryk <137694687+tatyanaskoryk@users.noreply.github.com>
2023-11-02 14:43:58 +0200

    add changehelp

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 import * as basicLightbox from 'basiclightbox';
+galleryList.addEventListener('click', (event) => {
+  event.preventDefault();
 
+  if (event.target.nodeName === 'IMG') {
+    const largeImageURL = event.target.dataset.source;
+
+    const lightbox = basicLightbox.create(`
+      <img src="${largeImageURL}" alt="${event.target.alt}" />
+    `);
+
+    lightbox.show();
+  }
+});
 console.log(galleryItems);
