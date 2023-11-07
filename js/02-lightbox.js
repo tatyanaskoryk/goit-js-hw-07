521a3f6cd67579e8bff7a52ed9a3d73a7925630f
tatyanaskoryk <137694687+tatyanaskoryk@users.noreply.github.com>
2023-11-07 18:18:30 +0200

    add grygoriy

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,17 +1,5 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-galleryList.addEventListener('click', (event) => {
-  event.preventDefault();
 
-  if (event.target.nodeName === 'IMG') {
-    const largeImageURL = event.target.dataset.source;
-
-    const lightbox = basicLightbox.create(`
-      <img src="${largeImageURL}" alt="${event.target.alt}" />
-    `);
-
-    lightbox.show();
-  }
-});
 console.log(galleryItems);
