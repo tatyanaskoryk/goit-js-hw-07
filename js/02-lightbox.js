e6fe867c21dbd9e7bb9d6f92c604aa7c106dbf13
tatyanaskoryk <137694687+tatyanaskoryk@users.noreply.github.com>
2023-11-11 10:09:02 +0200

    add

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
+
+function getGallery() {
+  const galleryItemsMarkup = galleryItems
+    .map(
+      ({ preview, original, description }) =>
+       `<li class="gallery__item">
+   <a class="gallery__link" href="large-image.jpg">
+      <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+   </a>
+</li>`
+    )
+    .join('');
+
+  galleryList.innerHTML = galleryItemsMarkup;
+}
+
+getGallery();
+
+galleryList.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const largeImageURL = event.target.dataset.source;
+
+  const lightbox = basicLightbox.create(
+    `<img src="${largeImageURL}" alt="${event.target.alt}" />`
+  );
+
+  lightbox.show();
+});
 
 
 console.log(galleryItems);
